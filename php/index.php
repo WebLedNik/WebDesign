5a7ab23231781569389a2d4960ec99c6bd69273f
WebLedNik <nik@led.moscow>
2020-08-18 23:49:43 +0300

    Add file

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	
+</body>
+</html>
